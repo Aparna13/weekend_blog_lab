d2350517c0c1315235d6adc122c953e2b79e9a78
Alex Notov <alex@zealoushacker.com>
2015-02-20 14:40:40 -0800

    Model validations and relationships

--- blog-lab-app/app/models/author.rb
+++ blog-lab-app/app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  has_many :posts
+
+  validates_length_of :first_name, :last_name, minimum: 4, maximum: 20
+  validates_numericality_of :age
 end
--- blog-lab-app/app/models/post.rb
+++ blog-lab-app/app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+  
+  validates_presence_of :title, :body, :category
+  validates_length_of :title, minimum: 3
 end
