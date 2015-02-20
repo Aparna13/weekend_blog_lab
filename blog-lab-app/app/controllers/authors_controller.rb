c9edcdef383c65a6a03557e0466e1fa2cb5e947b
Alex Notov <alex@zealoushacker.com>
2015-02-20 15:57:29 -0800

    Author form

--- blog-lab-app/app/controllers/authors_controller.rb
+++ blog-lab-app/app/controllers/authors_controller.rb
@@ -69,6 +69,6 @@ class AuthorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def author_params
-      params[:author]
+      params.require(:author).permit(:first_name, :last_name, :age)
     end
 end
